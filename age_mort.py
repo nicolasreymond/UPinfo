bdf91f9851efb5fa2dd032a733a94e768ccefe54
nicolasreymond <n.reymond.12@gmail.com>
2024-08-27 13:32:10 +0200

    [refactore] Add header

--- age_mort.py
+++ age_mort.py
@@ -1,7 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ---------------------------------------------------------------------------
+# Created By  : Nicolas Reymond
+# Created Date: 27/08/2024
+# version ='1.0'
+# ---------------------------------------------------------------------------
+
 # init var and ask for data (or init with default data)
 birth_year = int(input("Saisir l'année de naissance: ") or 1868)
 death_year = int(input("Saisir l'année de mort: ") or 1921)
+
 # Calculate death age
 death_age = death_year - birth_year
 # Print result
+
 print("\ndate de naissance : " + str(birth_year) + "\ndate de mort : " + str(death_year) + "\nage de mort : " + str(death_age))
