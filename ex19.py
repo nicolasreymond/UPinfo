5aa9d92cfc9384a10e8c6c753ca347ef03ba0170
nicolasreymond <n.reymond.12@gmail.com>
2024-08-28 12:06:36 +0200

    [Add] ex19

---
+++ ex19.py
@@ -0,0 +1,27 @@
+import random
+
+# The computer randomly selects a number between 0 and 100
+secret_number = random.randint(0, 100)
+
+# Set the maximum number of attempts
+max_attempts = 10
+
+print(f"The computer has chosen a number between 0 and 100. Let's try to find it in less than {max_attempts} steps.")
+
+for attempt in range(1, max_attempts + 1):
+    # The computer makes a guess
+    guess = int(input("Enter a guess : "))
+
+    # Check the guess
+    if guess < secret_number:
+        print("It's higher!")
+        low = guess + 1
+    elif guess > secret_number:
+        print("It's lower!")
+        high = guess - 1
+    else:
+        print(f"Win !! \n The secret number {secret_number} in {attempt} attempts.")
+        break
+else:
+    print(f"Lose !! Couldn't find the number in {max_attempts} attempts.")
+    print(f"The secret number was {secret_number}.")
