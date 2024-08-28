cbaa04a2e65dda9316befcf1db008c0cef20b0a6
nicolasreymond <n.reymond.12@gmail.com>
2024-08-28 11:48:15 +0200

    [change] Add func

--- ex17.py
+++ ex17.py
@@ -1,6 +1,9 @@
-for n in range(1, 11):
+def table_of_n(n):
     print(f"Table of {n}")
     for i in range(1, 11, 1):
         val = n * i
         print(f'{n} * {i:2d} = {val:2d}')
     print("")
+
+for n in range(1, 11):
+    table_of_n(n)
